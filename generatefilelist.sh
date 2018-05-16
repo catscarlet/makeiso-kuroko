df555699047eaf643e14074bdef603b15254611f
catscarlet <i@catscarlet.com>
2018-05-17 00:02:32 +0800

    Update generatefilelist.sh and fix a file name reference error.

--- generatefilelist.sh
+++ generatefilelist.sh
@@ -5,24 +5,26 @@ PACKAGE_PATH=/tmp/mountpoint/CentOS7-Everything/Packages
 COUNT=0
 FOUND=0
 NOT_FOUND=0
-FILELIST='filelist'
+FILELIST='filelist/your_rpm_payload.list'
 FILELIST_TEMP=`mktemp`
 rpm -qa | sort > $FILELIST_TEMP
-echo '临时文件：'$FILELIST_TEMP
+echo 'temp file: '$FILELIST_TEMP
 
 while read line
     do
         ((COUNT=COUNT+1))
         if [[ -f $PACKAGE_PATH/$line.rpm ]]; then
-            #echo '发现文件'$line.rpm
+            #echo 'Found '$line.rpm
             echo $line.rpm >> $FILELIST
             ((FOUND=FOUND+1))
         else
             ((NOT_FOUND=NOT_FOUND+1))
-            echo '未发现文件'$line.rpm
+            echo 'Not found '$line.rpm
         fi
     done < $FILELIST_TEMP
 
-echo '文件数：'$COUNT
-echo '发现文件数:'$FOUND
-echo '未发现文件数:'$NOT_FOUND
+echo 'Count: '$COUNT
+echo 'Hit: '$FOUND
+echo 'Miss: '$NOT_FOUND
+echo ''
+echo 'New filelist is generated to: '$FILELIST
