b8b086e276c7fd6f011db8273148c1bf3aa397e0
catscarlet <i@catscarlet.com>
2018-06-01 00:37:10 +0800

    Update generatefilelist.sh for file exist check

--- generatefilelist.sh
+++ generatefilelist.sh
@@ -6,9 +6,16 @@ COUNT=0
 FOUND=0
 NOT_FOUND=0
 FILELIST='filelist/your_rpm_payload.list'
+if [[ -s $FILELIST ]]; then
+    FILELIST_NEW='filelist/your_rpm_payload-'`date +'%Y%m%d%H%M%S'`'.list'
+    echo "Notice: $FILELIST already exist. Your RPM payload list will be generated in $FILELIST_NEW"
+    FILELIST=$FILELIST_NEW
+fi
+touch $FILELIST
 FILELIST_TEMP=`mktemp`
 rpm -qa | sort > $FILELIST_TEMP
-echo 'temp file: '$FILELIST_TEMP
+#echo 'temp file: '$FILELIST_TEMP
+echo ''
 
 while read line
     do
@@ -19,7 +26,7 @@ while read line
             ((FOUND=FOUND+1))
         else
             ((NOT_FOUND=NOT_FOUND+1))
-            echo 'Not found '$line.rpm
+            echo 'Not found: '$line.rpm
         fi
     done < $FILELIST_TEMP
 
