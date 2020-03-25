8a3f56ef9c05247590c40ce82dd6b1840a31fe60
Catscarlet <i@catscarlet.com>
2020-03-25 22:22:27 +0800

    Major update.

--- makeiso.sh
+++ makeiso.sh
@@ -6,14 +6,21 @@ STARTTIME=`date +%s`
 cd $(dirname $(readlink -f $0))
 
 # INPUT
-CENTOS7_EVERYTHING_ISO="/tmp/mountpoint/samba/share/CentOS-7-x86_64-Everything-1611.iso"
-PAYLOAD_PATH="/root/payload_test/"
+CENTOS7_EVERYTHING_ISO='/tmp/mountpoint/samba/share/CentOS-7-x86_64-Everything-1611.iso'
+CENTOS7_EVERYTHING_ISO_MOUNTPOINT='/tmp/mountpoint/CentOS7-Everything/'
+PAYLOAD_PATH='./payload_sample/'
 CONFIGDIR='boot.template/develop/'
 
 # OUTPUT
-OUTPUTFILEDIR="./"
-VERSION="v1.0.0"
+OUTPUTFILEDIR='./'
+VERSION='v1.0.0'
 TIMEZONE='UTC'
+
+# Global variables
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+ISOTMP='./iso_tmp'
 
 usage()
 {
@@ -21,7 +28,12 @@ usage()
     exit 1
 }
 
-while getopts "d:v:s:7:z:h" arg
+print_horizontal_line()
+{
+    printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+}
+
+while getopts 'd:v:s:7:z:h' arg
 do
     case $arg in
         d)
@@ -36,6 +48,9 @@ do
         7)
             CENTOS7_EVERYTHING_ISO=$OPTARG
             ;;
+        t)
+            ISOTMP=$OPTARG
+            ;;
         z)
             TIMEZONE=$OPTARG
             ;;
@@ -43,78 +58,79 @@ do
             usage
             ;;
         *)  #unkonw argument?
-            echo "unkonw argument"
+            echo -e $RED'unkonw argument'$NC
             usage
             ;;
     esac
 done
 
-# AUTO VARIABLE
-VOLUMENAME='PAYLOAD-'`date +'%Y%m%d%H%M'`-$VERSION
-VOLUMENAME_SHORT=`expr substr ${VOLUMENAME} 1 16`
+# Auto generated variables
+VOLUMENAME='PAYLOAD-'`date +'%Y%m%d%H%M%S'`-$VERSION
+VOLUMENAME_LABEL=`expr substr ${VOLUMENAME} 1 16`
 FINALNAME=${VOLUMENAME}.iso
 
-# Check
-if [[ `createrepo --version 1>/dev/null 2>/dev/null` ]]; then
-    echo 'createrepo not installed. Exit.'
-    exit 127
-fi
+# Before start. Check environment
+echo ''
+command -v rsync >/dev/null 2>&1 || { echo -e >&2 $RED'rsync is not installed.  Aborting.'$NC; exit 1; }
+command rsync -n --info=progress2 rsync-3.1.2-5.fc26.x86_64.rpm ${ISOTMP}/Packages >/dev/null 2>&1 || { echo -e >&2 $RED'This version of rsync is not supported. Version 3.1.1+ is recommended. Please upgrade.  Aborting.'$NC; exit 1; }
+command -v createrepo >/dev/null 2>&1 || { echo -e >&2 $RED'createrepo is not installed.  Aborting.'$NC; exit 1; }
+command -v genisoimage >/dev/null 2>&1 || { echo -e >&2 $RED'genisoimage is not installed.  Aborting.'$NC; exit 1; }
 
-if [[ `genisoimage --version 1>/dev/null 2>/dev/null` ]]; then
-    echo 'genisoimage not installed. Exit.'
-    exit 127
-fi
 
-mkdir -p /tmp/mountpoint/CentOS7-Everything/
-mountpoint /tmp/mountpoint/CentOS7-Everything/ >/dev/null ||
-if [[ !`mountpoint /tmp/mountpoint/CentOS7-Everything/ >/dev/null` ]]; then
-    echo 'Try mount '${CENTOS7_EVERYTHING_ISO} && mount -t iso9660 -o loop ${CENTOS7_EVERYTHING_ISO} /tmp/mountpoint/CentOS7-Everything/
+# Start
+mkdir -p ${CENTOS7_EVERYTHING_ISO_MOUNTPOINT}/
+mountpoint ${CENTOS7_EVERYTHING_ISO_MOUNTPOINT}/ >/dev/null ||
+if [[ !`mountpoint ${CENTOS7_EVERYTHING_ISO_MOUNTPOINT}/ >/dev/null` ]]; then
+    echo 'Try mount '${CENTOS7_EVERYTHING_ISO} && mount -t iso9660 -o loop ${CENTOS7_EVERYTHING_ISO} ${CENTOS7_EVERYTHING_ISO_MOUNTPOINT}/
 fi
 
 if [[ ! -d $PAYLOAD_PATH || ! -f  $PAYLOAD_PATH/install.sh ]]; then
-    echo 'Payload not found in '$PAYLOAD_PATH'. Exit.'
+    echo -e $RED'Payload not found in '$PAYLOAD_PATH'. Exit.'$NC
     exit 1
 fi
 
-echo "----------------------------------------------------------------------------"
-echo Start to generate $FINALNAME at  `date -d "1970-01-01 UTC $STARTTIME seconds" +"%Y-%m-%d %T %z"`
-
-echo "----------------------------------------------------------------------------"
-rm -rf ./iso_tmp
+print_horizontal_line
+echo -e $GREEN Start to generate $FINALNAME at  `date -d "1970-01-01 UTC $STARTTIME seconds" +'%Y-%m-%d %T %z'` $NC
+print_horizontal_line
+echo 'Clear ISO temporary files at '${ISOTMP}'.'
+rm -rf ${ISOTMP}
 echo 'Copy CentOS7 DVD framework'
-mkdir -p ./iso_tmp/Packages
-mkdir -p ./iso_tmp/repodata
-mkdir -p ./iso_tmp/PAYLOAD
-rsync -au --info=progress2 /tmp/mountpoint/CentOS7-Everything/isolinux iso_tmp/
-xargs -n 1 -i cp -rs /tmp/mountpoint/CentOS7-Everything/{} ./iso_tmp < ./filelist/centos_dvd_frame.list
+mkdir -p ${ISOTMP}/Packages
+mkdir -p ${ISOTMP}/repodata
+mkdir -p ${ISOTMP}/PAYLOAD
+rsync -au --info=progress2 ${CENTOS7_EVERYTHING_ISO_MOUNTPOINT}/isolinux iso_tmp/
+xargs -n 1 -i cp -rs ${CENTOS7_EVERYTHING_ISO_MOUNTPOINT}/{} ${ISOTMP} < ./filelist/centos_dvd_frame.list
 
 echo -e 'Copy(Actually it'"'"'s "make symbolic links") minimal packages from CentOS7 DVD'
-#rsync -a --info=progress2 --files-from=./filelist/minimal.list /tmp/mountpoint/CentOS7-Everything/Packages ./iso_tmp/Packages
-xargs -n 1 -i cp -s /tmp/mountpoint/CentOS7-Everything/Packages/{} ./iso_tmp/Packages < ./filelist/minimal.list
+#rsync -a --info=progress2 --files-from=./filelist/minimal.list ${CENTOS7_EVERYTHING_ISO_MOUNTPOINT}/Packages ${ISOTMP}/Packages
+xargs -n 1 -i cp -s ${CENTOS7_EVERYTHING_ISO_MOUNTPOINT}/Packages/{} ${ISOTMP}/Packages < ./filelist/minimal.list
 
 echo 'Copy rsync-3.1.2-5.fc26.x86_64.rpm'
-rsync -a --info=progress2 rsync-3.1.2-5.fc26.x86_64.rpm ./iso_tmp/Packages
+rsync -a --info=progress2 rsync-3.1.2-5.fc26.x86_64.rpm ${ISOTMP}/Packages
 
 echo 'Copy PAYLOAD'
-rsync -a --info=progress2 ${PAYLOAD_PATH} ./iso_tmp/PAYLOAD
+rsync -a --info=progress2 ${PAYLOAD_PATH} ${ISOTMP}/PAYLOAD
 
-echo 'Copy boot files'
-rsync -a --info=progress2 $CONFIGDIR/ ./iso_tmp/
-sed -i 's/{$TITLE}/'$VOLUMENAME'/' ./iso_tmp/isolinux/isolinux.cfg
-sed -i 's/{$TITLE}/'$VOLUMENAME'/' ./iso_tmp/EFI/BOOT/grub.cfg
-#sed -i 's/{$VOLUMENAME_SHORT}/'$VOLUMENAME_SHORT'/' ./iso_tmp/isolinux/isolinux.cfg
-sed -i 's/{$VOLUMENAME_SHORT}/'$VOLUMENAME_SHORT'/' ./iso_tmp/EFI/BOOT/grub.cfg
+echo 'Copy Boot files'
+rsync -a --info=progress2 $CONFIGDIR/ ${ISOTMP}/
+sed -i 's/{$TITLE}/'$VOLUMENAME'/' ${ISOTMP}/isolinux/isolinux.cfg
+sed -i 's/{$TITLE}/'$VOLUMENAME'/' ${ISOTMP}/EFI/BOOT/grub.cfg
+#sed -i 's/{$VOLUMENAME_LABEL}/'$VOLUMENAME_LABEL'/' ${ISOTMP}/isolinux/isolinux.cfg
+sed -i 's/{$VOLUMENAME_LABEL}/'$VOLUMENAME_LABEL'/' ${ISOTMP}/EFI/BOOT/grub.cfg
 escaped_x="$(sed -e 's/[\/&]/\\&/g' <<< "$TIMEZONE")"
 ESCAPED_TIMEZONE="${escaped_x}"
-sed -i 's/{$TIMEZONE}/'$ESCAPED_TIMEZONE'/' ./iso_tmp/isolinux/payload-develop.cfg
+sed -i 's/{$TIMEZONE}/'$ESCAPED_TIMEZONE'/' ${ISOTMP}/isolinux/payload-develop.cfg
 
 echo 'Create CentOS7 comps.xml'
-rsync -a --info=progress2 83b61f9495b5f728989499479e928e09851199a8846ea37ce008a3eb79ad84a0-c7-minimal-x86_64-comps.xml ./iso_tmp/repodata
-createrepo -g repodata/83b61f9495b5f728989499479e928e09851199a8846ea37ce008a3eb79ad84a0-c7-minimal-x86_64-comps.xml ./iso_tmp
+rsync -a --info=progress2 83b61f9495b5f728989499479e928e09851199a8846ea37ce008a3eb79ad84a0-c7-minimal-x86_64-comps.xml ${ISOTMP}/repodata
+createrepo -g repodata/83b61f9495b5f728989499479e928e09851199a8846ea37ce008a3eb79ad84a0-c7-minimal-x86_64-comps.xml ${ISOTMP}
 
-echo Generate ISO file in $OUTPUTFILEDIR/$FINALNAME
+echo ''
+echo Generating ISO file in $OUTPUTFILEDIR/$FINALNAME
 echo Version：$VERSION
 echo Volume name：$VOLUMENAME
+echo Volume label: $VOLUMENAME_LABEL
+
 mkdir -p $OUTPUTFILEDIR
 genisoimage \
     -quiet \
@@ -131,18 +147,23 @@ genisoimage \
     -b images/efiboot.img \
     -no-emul-boot \
     -input-charset utf-8 \
-    -V $VOLUMENAME_SHORT \
+    -V $VOLUMENAME_LABEL \
     -o $OUTPUTFILEDIR/$FINALNAME \
-    ./iso_tmp
+    ${ISOTMP}
 
-echo 'Umount /tmp/mountpoint/CentOS7-Everything/'
-umount /tmp/mountpoint/CentOS7-Everything/
+echo "ISO is generated. Umount ${CENTOS7_EVERYTHING_ISO_MOUNTPOINT}/"
+# umount doesn't matter
+umount ${CENTOS7_EVERYTHING_ISO_MOUNTPOINT}/ ||
 
-echo 'ISO is generated.'
-ls -l --color=auto $OUTPUTFILEDIR/$FINALNAME
+echo ''
+print_horizontal_line
+echo -e $GREEN'ISO is generated at: '$NC $(realpath $OUTPUTFILEDIR/$FINALNAME)
+ls -lh --color=auto $OUTPUTFILEDIR/$FINALNAME
 ENDTIME=`date +%s`
 SPENTTIME=$(($ENDTIME-$STARTTIME))
+echo -e $GREEN'Finished. Time usage: '$SPENTTIME' seconds'$NC
+print_horizontal_line
 
-echo "Finished. Used time: $SPENTTIME seconds"
-
-#clean_up
+#clean_up_tips
+echo 'ISO temporary files is located at '${ISOTMP}'. You may want to remove it.'
+echo ''
